6b5969c4b08f17e6b3d7ba6cc9b032b5b20b99f3
qmaster0803 <qmaster080305@gmail.com>
2021-02-18 13:59:14 +0300

    v1.3 release

--- main.py
+++ main.py
@@ -22,8 +22,14 @@ def queryYN(question):
 def showStreamSelector(video, calcMaxRes=False, media_type=3):
     video_stream = None
     audio_stream = None
-    video_codec_type = None
-    audio_codec_type = None
+    result_codec_type = None
+    video_codec_type = 0 #for internal calculations
+    audio_codec_type = 0 #for internal calculations
+    #CODEC TYPES
+    #-1 = only audio in mp4a codec
+    #-2 = only audio in opus codec
+    #1 = video&audio in mp4 container
+    #2 = video&audio in webm container
     print("Choose stream:")
     while(True): #this loop is for returning user on choose again, if audio and video codecs in PRO mode can't be stored in one file without re-encoding
         streams = []
@@ -33,7 +39,7 @@ def showStreamSelector(video, calcMaxRes=False, media_type=3):
             if(stream.type == "video" and stream.is_progressive == False):
                 video_codec = stream.parse_codecs()[0].split(".")[0]
                 show_stream = False
-                if(media_type == 1 and (video_codec == "avc1" or video_codec == "av01")): show_stream = True
+                if(media_type == 1 and video_codec == "av01"): show_stream = True
                 if(media_type == 2 and video_codec == "vp9"): show_stream = True
                 if(media_type == 3): show_stream = True
                 if(show_stream):
@@ -63,48 +69,84 @@ def showStreamSelector(video, calcMaxRes=False, media_type=3):
                     streams.append(stream)
                     print(str(i)+") Audio;", "Bitrate: "+stream.abr, "Codec: "+stream.parse_codecs()[1].split(".")[0])
         #Requesting user's choise
-        #TODO audio_stream
-        if(media_type == 3):
-            video_stream_num = doUserInputNumber(len(streams)-1, "Enter VIDEO stream number: ")
-            video_stream = streams[video_stream_num]
+        if(media_type == 1): #in MP4/MP3 mode
+            video_stream_num = doUserInputNumber(len(streams)-1, "Choose stream: ")
+            if(streams[video_stream_num].type == "video"):
+                video_stream = streams[video_stream_num]
+                audio_stream = selectAudioForVideo(video, video_stream)
+            else:
+                audio_stream = streams[video_stream_num]
+        if(media_type == 2): #in WEBM/OGG mode
+            video_stream_num = doUserInputNumber(len(streams)-1, "Choose stream: ")
+            if(streams[video_stream_num].type == "video"):
+                video_stream = streams[video_stream_num]
+                audio_stream = selectAudioForVideo(video, video_stream)
+            else:
+                audio_stream = streams[video_stream_num]
+        if(media_type == 3): #PRO mode
+            video_stream_num = doUserInputNumber(len(streams)-1, "Enter VIDEO stream number (Enter zero to donwload only audio): ", allow_zero=True)
+            if(video_stream_num != -1):
+                video_stream = streams[video_stream_num]
             audio_stream_num = doUserInputNumber(len(streams)-1, "Enter AUDIO stream number: ")
             audio_stream = streams[audio_stream_num]
 
-        if(video_stream.parse_codecs()[0].split(".")[0] == "avc1" or video_stream.parse_codecs()[0].split(".")[0] == "av01"): video_codec_type = 1
-        if(video_stream.parse_codecs()[0].split(".")[0] == "vp9"): video_codec_type = 2
+        if(video_stream != None): #detecting video codec (none for audio only mode)
+            if(video_stream.parse_codecs()[0].split(".")[0] == "avc1" or video_stream.parse_codecs()[0].split(".")[0] == "av01"): video_codec_type = 1
+            if(video_stream.parse_codecs()[0].split(".")[0] == "vp9"): video_codec_type = 2
+        else:
+            video_stream = 0
 
+        #detecting audio codec
         if(audio_stream.parse_codecs()[1].split(".")[0] == "mp4a"): audio_codec_type = 1
         if(audio_stream.parse_codecs()[1].split(".")[0] == "opus"): audio_codec_type = 2
 
-        if(video_codec_type == audio_codec_type):
+        #if codec types can be stored in one file, or only audio selected
+        if(video_codec_type == audio_codec_type or video_codec_type == 0):
             break
         else:
             print("\n"*4)
             print("Chosen codecs can't be stored in one file without re-encoding. This program can't be used as re-encoder. Please, select other streams.")
 
-    if(calcMaxRes): return video_stream, audio_stream, codec_type, max_res
-    else: return video_stream, audio_stream, codec_type
+    print("video_codec", video_codec_type)
+    print("audio_codec", audio_codec_type)
+    if(video_codec_type == 0 and audio_codec_type == 1): result_codec_type = -1
+    if(video_codec_type == 0 and audio_codec_type == 2): result_codec_type = -2
+    if(video_codec_type == 1): result_codec_type = 1
+    if(video_codec_type == 2): result_codec_type = 2
+    if(calcMaxRes): return video_stream, audio_stream, result_codec_type, max_res
+    else: return video_stream, audio_stream, result_codec_type
 
 def selectAudioForVideo(vid, video_stream):
     video_codec = video_stream.parse_codecs()[0].split(".")[0]
     audio_stream = None
     if(video_codec == "avc1" or video_codec == "av01"): preferred_audio_codec = "mp4a"
     if(video_codec == "vp9"): preferred_audio_codec = "opus"
+    print("Preff audio:", preferred_audio_codec)
     for stream in vid.streams:
-        if(stream.type == "audio" and stream.parse_codecs()[1].split(".")[0] == "mp4a"):
+        if(stream.type == "audio" and stream.parse_codecs()[1].split(".")[0] == preferred_audio_codec):
             audio_stream = stream
     return audio_stream
 
-def downloadFile(url, name):
-    downloader.download(url, os.getcwd(), output=name, lflag=5)
-    idm_folder_name = urllib.parse.urlparse(url).netloc.split(".")[0]
-    found = True
-    while(found):
-        found = False
-        dirlist = os.listdir(os.path.join(os.getenv('APPDATA'), "IDM\\DwnlData\\", getpass.getuser()))
-        for i in dirlist:
-            if(i[:len(idm_folder_name)] == idm_folder_name):
-                found = True
+def downloadFile(url, name, IDM_mode, autocopy):
+    if(IDM_mode):
+        downloader.download(url, os.getcwd(), output=name, lflag=5)
+        idm_folder_name = urllib.parse.urlparse(url).netloc.split(".")[0]
+        found = True
+        while(found):
+            found = False
+            dirlist = os.listdir(os.path.join(os.getenv('APPDATA'), "IDM\\DwnlData\\", getpass.getuser()))
+            for i in dirlist:
+                if(i[:len(idm_folder_name)] == idm_folder_name):
+                    found = True
+    else:
+        if(autocopy):
+            print("Link generated and copied to clipboard. Please save it with", os.path.splitext(name)[1], "extension.")
+            pyperclip.copy(url)
+        else:
+            print("Link generated. Please save it with", os.path.splitext(name)[1], "extension.")
+            print("-"*20)
+            print(url)
+            print("-"*20)
 
 def mergeStreams(input_video, input_audio, output_video):
     video_stream = ffmpeg.input(input_video).video
@@ -114,12 +156,16 @@ def mergeStreams(input_video, input_audio, output_video):
     os.remove(input_video)
     os.remove(input_audio)
 
-def doUserInputNumber(max_val, text="Enter number: "):
+def doUserInputNumber(max_val, text="Enter number: ", allow_zero=False):
     while(True):
         try:
             res = int(input(text))-1
-            if(res >= 0 and res <= max_val):
-                break
+            if(allow_zero):
+                if(res >= -1 and res <= max_val):
+                    break
+            else:
+                if(res >= 0 and res <= max_val):
+                    break
         except ValueError:
             pass
     return res
@@ -162,34 +208,57 @@ while(True):
             print("WARNING! In PRO mode all codecs will be shown, so output files can be corrupted if you made wrong choise.")
         if(media_type > 0 and media_type < 4): break
     except ValueError: pass
-print("\n"*4)
+print()
 
-if(not playlist):
+if(playlist):
+    playlist = Playlist(url)
+    if(len(playlist) == 1):
+        print("Got 1 video")
+    else:
+        print("Got", len(playlist), "videos")
+    stream, audio_stream, codec_type = showStreamSelector(playlist, media_type=media_type)
+else:
     vid = YouTube(url)
     print(vid.title)
 
     stream, audio_stream, codec_type = showStreamSelector(vid, media_type=media_type)
     
-    url = stream.url
+    print("codec:", codec_type)
+    progressive_output = False
+    if(codec_type == -1): #only audio in mp4a codec
+        print("Downloading audio... ", end='', flush=True)
+        url = audio_stream.url
+        downloadFile(url, vid.title+".mp3", IDM_mode, autocopy)
+        if(IDM_mode): print("done.")
+    if(codec_type == -2): #only audio in opus codec
+        print("Downloading audio... ", end='', flush=True)
+        url = audio_stream.url
+        downloadFile(url, vid.title+".ogg", IDM_mode, autocopy)
+        if(IDM_mode): print("done.")
+
     if(codec_type == 1):
+        progressive_output = True
         video_extension = ".mp4"
         audio_extension = ".mp3"
     if(codec_type == 2):
+        progressive_output = True
         video_extension = ".webm"
         audio_extension = ".ogg"
-    if(stream.type == "video"):
-        print("Downloading video... ", end='', flush=True)
-        downloadFile(url, "temp"+video_extension)
-        print("done.", flush=True)
-        print("Downloading audio... ", end='', flush=True)
-        url = audio_stream.url
-        downloadFile(url, "temp"+audio_extension)
-        print("done.")
-        print("Merging streams... ", end='', flush=True)
-        mergeStreams("temp"+video_extension, "temp"+audio_extension, vid.title+video_extension)
-        print("done.")
-    if(stream.type == "audio"):
-        print("Downloading audio... ", end='', flush=True)
-        downloadFile(url, vid.title+audio_extension)
-        print("done.")
+    if(progressive_output):
+        video_url = stream.url
+        audio_url = audio_stream.url
+        if(IDM_mode):
+            print("Downloading video... ", end='', flush=True)
+            downloadFile(video_url, "temp"+video_extension, IDM_mode, autocopy)
+            print("done.", flush=True)
+            print("Downloading audio... ", end='', flush=True)
+            downloadFile(audio_url, "temp"+audio_extension, IDM_mode, autocopy)
+            print("done.")
+            print("Merging streams... ", end='', flush=True)
+            mergeStreams("temp"+video_extension, "temp"+audio_extension, vid.title+video_extension)
+            print("done.")
+        else:
+            downloadFile(video_url, "temp"+video_extension, IDM_mode, autocopy)
+            input("Press enter to generate next link.")
+            downloadFile(audio_url, "temp"+audio_extension, IDM_mode, autocopy)            
     print("Done\n")
